0cd8c49dacc0fe8241e16d704f718a842af7c633
zhanghengyang <danielzhang-1@163.com>
2015-03-10 12:49:45 +0800

    second

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SenzApiServiceFabFile',
+    version='0.1',
+    packages=['fab_tyk_deploy', 'fab_docker_service_deploy'],
+    url='',
+    license='GPL',
+    author='zhanghengyang',
+    author_email='bboalimoe@gmail.com',
+    description='fabfile '
+)
